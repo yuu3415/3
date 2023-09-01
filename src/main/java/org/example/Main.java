0ab6120f561b37d33bb6f99ed6db80f936468c7a
yuu3415 <buddyyou6@gmail.com>
2023-09-01 10:07:42 +0900

    first

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,45 @@
 package org.example;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> names = new ArrayList<>();
+        names.add("メッシ");
+        names.add("クリロナ");
+        names.add(null);
+        names.add("イニエスタ");
+
+        System.out.println(names);
+        System.out.println(names.toArray().length);
+
+
+        Map<String, String> userMap = new HashMap<>();
+        userMap.put("マリエ", "とてもうるさいです");
+        userMap.put(null, null);
+        userMap.put("りさこ", "・・・");
+        userMap.remove("りさこ");
+
+
+        System.out.println(userMap);
+
+
+        List<String> dogs = new ArrayList<>();
+        dogs.add("くろ");
+        dogs.add(null);
+        dogs.add("ポチ");
+
+        System.out.println(dogs);
+        System.out.println(dogs.toArray().length);
+
+        String name5 = null;
+        if (name5 == null) {
+            System.out.println("ここは空です");
+        }
+        Optional<String> value = Optional.ofNullable(name5);
+        System.out.println(value.or(() -> Optional.of("空です")).get());
+
+        System.out.println(name5.length());
     }
+
 }
